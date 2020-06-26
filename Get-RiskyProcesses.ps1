869e72f731517c444830be75cc1b1a8e4cfa8496
SturdyErde <samuel.erde@gmail.com>
2020-06-26 06:40:35 -0400

    Updated description

--- Get-RiskyProcesses.ps1
+++ Get-RiskyProcesses.ps1
@@ -3,21 +3,21 @@
     Checks running processes for a list of "risky" processes that should not be spawned by "risky" parent processes.
 
     .DESCRIPTION
-    A blog post by the Microsoft Defender ATP Research Team on June 24, 2020 detailed some scenarios in which an 
-    attacker might exploit a remote code execution (RCE) vulnerability in the Internet Information Service (IIS) 
-    component of an Exchange Server, and thereby gain system privileges. 
-    See: https://www.microsoft.com/security/blog/2020/06/24/defending-exchange-servers-under-attack/
-
-    While Windows Defender ATP or other endpoint detection and response (EDR) products may natively be able to 
-    detect such behavior, systems without those protections may not. This script provides a working concept that 
-    could notify admins of these potential exploits, when the script is run as a scheduled task or when used in 
-    conjunction with a monitoring platform such as SolarWinds Orion.
+    A blog post by the Microsoft Defender ATP Research Team on June 24, 2020 detailed some scenarios in which an attacker 
+    might exploit a remote code execution (RCE) vulnerability in IIS component of an Exchange Server, and thereby gain 
+    system privileges. See: https://www.microsoft.com/security/blog/2020/06/24/defending-exchange-servers-under-attack/. 
+    One indication of such an exploit might be a "cmd.exe" process (among others) that is spawned by "w3wp.exe", or the 
+    IIS application pool. 
+    
+    While Windows Defender ATP or other endpoint detection and response (EDR) products may natively be able to detect 
+    such behavior, systems without those protections may not. This script provides a working concept that could notify 
+    admins of these potential exploits, when the script is run as a scheduled task or when used in conjunction with a 
+    monitoring platform such as SolarWinds Orion.
 
     .NOTES
     Name: Get-RiskyProcesses
     Author: Sam Erde
     Last Modified: June 26, 2020
-    Resources: https://www.microsoft.com/security/blog/2020/06/24/defending-exchange-servers-under-attack/
 
     .LINK
     https://github.com/SturdyErde/Get-RiskyProcesses
